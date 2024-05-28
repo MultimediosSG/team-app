970fd0418c4766f4048b57d22e3dcf838c42e0fd
Alonso Chavarria <alons182@hotmail.com>
2024-05-28 08:35:02 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="container mx-auto px-4 py-6">
+    <RouterView />
+  </div>
+</template>
+
